2f8e1a3a600909c7a42669378f364b3b32187f79
gantheory <shunyaoshih@gmail.com>
2020-06-03 20:46:05 -0700

    Add markovization

--- src/edu/berkeley/nlp/assignments/parsing/student/GenerativeParserFactory.java
+++ src/edu/berkeley/nlp/assignments/parsing/student/GenerativeParserFactory.java
@@ -4,6 +4,7 @@ import edu.berkeley.nlp.assignments.parsing.Grammar;
 import edu.berkeley.nlp.assignments.parsing.SimpleLexicon;
 import edu.berkeley.nlp.assignments.parsing.TreeAnnotations;
 import edu.berkeley.nlp.assignments.parsing.UnaryClosure;
+import edu.berkeley.nlp.util.Counter;
 import edu.berkeley.nlp.util.CounterMap;
 import edu.berkeley.nlp.util.Indexer;
 import edu.berkeley.nlp.math.SloppyMath;
@@ -21,18 +22,21 @@ import edu.berkeley.nlp.ling.Trees.PennTreeRenderer;
 
 class GenerativeParser implements Parser
 {
-	final boolean debug = false;
-	final double EPSILON = 1e-9;
-	final double MIN_LOG_PROB = Double.NEGATIVE_INFINITY; // Math.log(EPSILON);
+	final double MIN_LOG_PROB = Double.NEGATIVE_INFINITY;
 	final int SENTENCE_LENGTH = 40;
-	final Tree<String> NULL_TREE = new Tree<String>("ROOT", Collections.singletonList(new Tree<String>("JUNK")));
+	final Tree<String> NULL_TREE = new Tree<>("ROOT", Collections.singletonList(new Tree<>("JUNK")));
 	final boolean useSloppyMath = false;
 	final boolean unFoldClosure = true;
+	final int v = 2;
+	final int h = 2;
+	final double COUNT_THRESHOLD = 10.0;
 
 	Grammar grammar;
 	SimpleLexicon lexicon;
 	UnaryClosure unaryClosure;
 	Indexer<String> indexer;
+	int rootID;
+	int numOfStates;
 
 	double[][] tagScore;
 	double[][][] unaryDP;
@@ -42,9 +46,9 @@ class GenerativeParser implements Parser
 	int[][][] binaryDPRightChild;
 	int[][][] binaryDPSplit;
 
+	Counter<String> stateCounter = new Counter<>();
 
 	public Tree<String> getBestParse(List<String> sentence) {
-		int numOfStates = indexer.size();
 		for (int state = 0; state < numOfStates; ++state) {
 			for (int i = 0; i < sentence.size(); ++i) {
 			  tagScore[state][i] = MIN_LOG_PROB;
@@ -64,7 +68,8 @@ class GenerativeParser implements Parser
 			for (int i = 0; i < sentence.size(); ++i) {
 			  // score could be NaN
 				double score = lexicon.scoreTagging(sentence.get(i), tag);
-				if (score != MIN_LOG_PROB && Double.isFinite(score)) tagScore[state][i] = score;
+				if (!isValidScore(score)) continue;
+				tagScore[state][i] = score;
 			}
 		}
 
@@ -72,11 +77,14 @@ class GenerativeParser implements Parser
 		  for (int i = 0; i < sentence.size(); ++i) {
 				for (UnaryRule rule : unaryClosure.getClosedUnaryRulesByParent(state)) {
 					double logRuleScore = rule.getScore();
+					if (!isValidScore(logRuleScore)) continue;
 					double logTerminalScore = tagScore[rule.getChild()][i];
-					if (logRuleScore == MIN_LOG_PROB || logTerminalScore == MIN_LOG_PROB) continue;
+					if (!isValidScore(logTerminalScore)) continue;
 					double logScore = myLogAdd(logRuleScore, logTerminalScore);
-          unaryDP[state][i][i] = logScore;
-					unaryDPChild[state][i][i] = rule.getChild();
+					if ((unaryDP[state][i][i] == MIN_LOG_PROB) || (logScore > unaryDP[state][i][i])) {
+						unaryDP[state][i][i] = logScore;
+						unaryDPChild[state][i][i] = rule.getChild();
+					}
 				}
 			}
 		}
@@ -87,17 +95,18 @@ class GenerativeParser implements Parser
 					int j = i + length - 1;
 					for (BinaryRule rule : grammar.getBinaryRulesByParent(state)) {
 						double logRuleScore = rule.getScore();
-						if (logRuleScore == MIN_LOG_PROB) continue;
+						if (!isValidScore(logRuleScore)) continue;
 						for (int k = i; k < j; ++k) {
 							double logLeftChildScore = unaryDP[rule.getLeftChild()][i][k];
+							if (!isValidScore(logLeftChildScore)) continue;
 							double logRightChildScore = unaryDP[rule.getRightChild()][k + 1][j];
-							if ((logLeftChildScore == MIN_LOG_PROB) || (logRightChildScore == MIN_LOG_PROB)) continue;
+							if (!isValidScore(logRightChildScore)) continue;
 							double logScore = myLogAdd(myLogAdd(logRuleScore, logLeftChildScore), logRightChildScore);
-              if ((binaryDP[state][i][j] == MIN_LOG_PROB) || (logScore > binaryDP[state][i][j])) {
-              	binaryDP[state][i][j] = logScore;
-              	binaryDPLeftChild[state][i][j] = rule.getLeftChild();
-              	binaryDPRightChild[state][i][j] = rule.getRightChild();
-              	binaryDPSplit[state][i][j] = k;
+							if ((binaryDP[state][i][j] == MIN_LOG_PROB) || (logScore > binaryDP[state][i][j])) {
+								binaryDP[state][i][j] = logScore;
+								binaryDPLeftChild[state][i][j] = rule.getLeftChild();
+								binaryDPRightChild[state][i][j] = rule.getRightChild();
+								binaryDPSplit[state][i][j] = k;
 							}
 						}
 					}
@@ -108,11 +117,11 @@ class GenerativeParser implements Parser
 					int j = i + length - 1;
 					for (UnaryRule rule : unaryClosure.getClosedUnaryRulesByParent(state)) {
 			  		double logRuleScore = rule.getScore();
-			  		if (logRuleScore == MIN_LOG_PROB) continue;
+			  		if (!isValidScore(logRuleScore)) continue;
 						double logChildScore = binaryDP[rule.getChild()][i][j];
-			  		if (logChildScore == MIN_LOG_PROB) continue;
+						if (!isValidScore(logChildScore)) continue;
 			  		double logScore = myLogAdd(logRuleScore, logChildScore);
-			  		if ((unaryDP[state][i][j] == MIN_LOG_PROB) || (logScore > unaryDP[state][i][j])) {
+						if ((unaryDP[state][i][j] == MIN_LOG_PROB) || (logScore > unaryDP[state][i][j])) {
 			  			unaryDP[state][i][j] = logScore;
 			  			unaryDPChild[state][i][j] = rule.getChild();
 						}
@@ -121,20 +130,13 @@ class GenerativeParser implements Parser
 			}
 		}
 
-    int rootID = indexer.indexOf("ROOT");
 		if (unaryDP[rootID][0][sentence.size() - 1] == MIN_LOG_PROB) return NULL_TREE;
 		Tree<String> parseTree = buildUnaryTree(rootID, 0, sentence.size() - 1, sentence);
-
-		if (debug) {
-			System.out.println("Success!");
-			System.out.println(PennTreeRenderer.render(parseTree));
-			System.out.println(PennTreeRenderer.render(TreeAnnotations.unAnnotateTree(parseTree)));
-		}
-
 		return TreeAnnotations.unAnnotateTree(parseTree);
 	}
 
 	private Tree<String> buildUnaryTree(int parent, int l, int r, List<String> sentence) {
+//		System.out.println("Unary: " + parent + " " + l + " " + r);
 		if (l == r) {
 			int preTerminal = unaryDPChild[parent][l][r];
 			Tree<String> treeWithTerminal = new Tree<>(indexer.get(preTerminal), Collections.singletonList(new Tree<>(sentence.get(l))));
@@ -148,7 +150,7 @@ class GenerativeParser implements Parser
 
 		UnaryRule rule = new UnaryRule(parent, child);
 		List<Integer> closurePath = unaryClosure.getPath(rule);
-		if (unFoldClosure && closurePath.size() > 2) {
+		if (unFoldClosure && closurePath != null && closurePath.size() > 2) {
 			Tree<String> prev = node;
 			for (int i = 1; i < closurePath.size() - 1; ++i) {
 				Tree<String> now = new Tree<>(indexer.get(closurePath.get(i)));
@@ -166,6 +168,7 @@ class GenerativeParser implements Parser
 	}
 
 	private Tree<String> buildBinaryTree(int parent, int l, int r, List<String> sentence) {
+//		System.out.println("Binary: " + parent + " " + l + " " + r);
 		if (l == r) return buildUnaryTree(parent, l, r, sentence);
 		Tree<String> node = new Tree<>(indexer.get(parent));
 		int mid = binaryDPSplit[parent][l][r];
@@ -192,7 +195,8 @@ class GenerativeParser implements Parser
 
 		indexer = grammar.getLabelIndexer();
 
-		int numOfStates = grammar.getLabelIndexer().size();
+		numOfStates = indexer.size();
+		rootID = indexer.indexOf("ROOT");
 
 		tagScore = new double[numOfStates][SENTENCE_LENGTH];
 		unaryDP = new double[numOfStates][SENTENCE_LENGTH][SENTENCE_LENGTH];
@@ -206,14 +210,125 @@ class GenerativeParser implements Parser
 	private List<Tree<String>> annotateTrees(List<Tree<String>> trees) {
 		List<Tree<String>> annotatedTrees = new ArrayList<Tree<String>>();
 		for (Tree<String> tree : trees) {
-			annotatedTrees.add(TreeAnnotations.annotateTreeLosslessBinarization(tree));
+			Tree<String> binarizedTree = binarization(tree);
+			annotatedTrees.add(binarizedTree);
+			countState(binarizedTree);
+		}
+		for (Tree<String> tree : annotatedTrees) {
+			collapseHorizontal(tree);
 		}
 		return annotatedTrees;
+	}
+
+	private Tree<String> binarization(Tree<String> tree) {
+		List<String> path = new ArrayList<>();
+		Tree<String> verticalizedTree = verticalMarkovization(tree, path);
+		Tree<String> horizontalizedTree = horizontalMarkovication(verticalizedTree, tree);
+		return horizontalizedTree;
+	}
+
+	private Tree<String> verticalMarkovization(Tree<String> tree, List<String> path) {
+		if (tree.isLeaf()) return tree;
+		path.add(tree.getLabel());
+		List<Tree<String>> newSubtrees = new ArrayList<>();
+		for (Tree<String> subtree : tree.getChildren()) {
+		  newSubtrees.add(verticalMarkovization(subtree, path));
+		}
+		path.remove(path.size() - 1);
+		StringBuilder nowLabel = new StringBuilder(tree.getLabel());
+		for (int i = 0; i < v - 1; ++i) {
+			if (path.size() - 1 - i < 0) break;
+			String label = path.get(path.size() - 1 - i);
+			nowLabel.append(String.format("^%s", label));
+		}
+		if (newSubtrees.size() == 1) {
+			if (!tree.getLabel().equals("ROOT") && !tree.isLeaf()) {
+				nowLabel.append("-U");
+			}
+			Tree<String> subtree = newSubtrees.get(0);
+			String label = subtree.getLabel();
+			if (label.equals("DT") || label.equals("RB")) {
+				subtree.setLabel(label + "^U");
+			}
+		}
+		return new Tree<>(nowLabel.toString(), newSubtrees);
+	}
+
+	private Tree<String> horizontalMarkovication(Tree<String> tree, Tree<String> originalTree) {
+	  if (tree.isLeaf()) return tree;
+		List<Tree<String>> oldSubtrees = tree.getChildren();
+		List<Tree<String>> newSubtrees = new ArrayList<>();
+		for (int i = 0; i < oldSubtrees.size(); ++i) {
+			newSubtrees.add(horizontalMarkovication(oldSubtrees.get(i), originalTree.getChildren().get(i)));
+		}
+		List<Tree<String>> newRoots = new ArrayList<>();
+		newRoots.add(new Tree<>(tree.getLabel()));
+		for (int i = 0; i < newSubtrees.size() - 1; ++i) {
+			StringBuilder nowLabel = new StringBuilder("@");
+			nowLabel.append(tree.getLabel());
+			nowLabel.append("[");
+			for (int j = 0; j < h; ++j) {
+				int idx = i - (h - 1) + j;
+				if (idx < 0) continue;
+				if (j == 0 && idx > 0) {
+					nowLabel.append("..._");
+				}
+				nowLabel.append(originalTree.getChildren().get(idx).getLabel());
+				nowLabel.append("_");
+			}
+			nowLabel.setLength(nowLabel.length() - 1);
+			nowLabel.append("]");
+			newRoots.add(new Tree<>(nowLabel.toString()));
+		}
+		for (int i = 0; i < newRoots.size(); ++i) {
+			if (i + 1 < newRoots.size()) {
+				newRoots.get(i).setChildren(new ArrayList<>(List.of(newSubtrees.get(i), newRoots.get(i + 1))));
+			} else {
+				newRoots.get(i).setChildren(Collections.singletonList(newSubtrees.get(i)));
+			}
+		}
+		return newRoots.get(0);
+	}
+
+	private void countState(Tree<String> tree) {
+		if (tree.isLeaf()) return;
+		List<Tree<String>> subtrees = tree.getChildren();
+	  for (Tree<String> subtree : subtrees) countState(subtree);
+		String key = tree.getLabel();
+		if (!stateCounter.containsKey(key)) {
+			stateCounter.setCount(key, 0.0);
+		}
+		stateCounter.incrementCount(key, 1.0);
+	}
+
+	private void collapseHorizontal(Tree<String> tree) {
+	  if (tree.isLeaf()) return;
+		for (Tree<String> subtree : tree.getChildren()) {
+			collapseHorizontal(subtree);
+		}
+	  String nowLabel = tree.getLabel();
+		double count = stateCounter.getCount(nowLabel);
+		if (count < COUNT_THRESHOLD) {
+			int begP = nowLabel.indexOf('[');
+			if (begP == -1) return;
+			int endP = nowLabel.lastIndexOf(']');
+			String[] labels = nowLabel.substring(begP + 1, endP).split("_");
+			if (labels.length < 2) return;
+			StringBuilder newLabel = new StringBuilder(nowLabel.substring(0, begP));
+			newLabel.append("[...");
+			for (int i = 1; i < labels.length; ++i)newLabel.append(String.format("_%s", labels[i]));
+			newLabel.append(nowLabel.substring(endP));
+			tree.setLabel(newLabel.toString());
+		}
 	}
 
 	private double myLogAdd(double a, double b) {
 		if (useSloppyMath) return SloppyMath.logAdd(a, b);
 		return a + b;
+	}
+
+	private boolean isValidScore(double a) {
+		return a != MIN_LOG_PROB && !Double.isNaN(a);
 	}
 }
 
